4ae92c9f7c4823b3b2901b4362634e3a95a6015e
Zain <zain@zainscity.com>
2025-12-07 21:54:03 +0500

    fix: Revert baseUrl to fix Vercel deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://Zainscity.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/AI_-_Humanoid_Robotics_Textbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
