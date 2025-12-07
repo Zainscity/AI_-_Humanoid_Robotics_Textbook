7a3694ff50e6f905f8112960958b3e48c3324b0e
Zain ul Abideen <mzainh06@gmail.com>
2025-12-07 22:28:07 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://Zainscity.github.io',
+  url: 'https://ai-humanoid-robotics-textbook.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Zainscity', // Usually your GitHub org/user name.
-  projectName: 'AI_-_Humanoid_Robotics_Textbook', // Usually your repo name.
+  projectName: 'humanoid_robotics_book', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
