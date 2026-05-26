3158cf228cf5bf6f0ad65d036e55b2daa0dd69fb
Zain ul Abideen <mzainh06@gmail.com>
2026-05-26 21:52:04 +0500

    comit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://humanoid-robotics-backend-sooty.vercel.app';
 
 export default API_BASE_URL;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://humanoid-robotics-backend-sooty.vercel.app';
 
 export default API_BASE_URL;
 
